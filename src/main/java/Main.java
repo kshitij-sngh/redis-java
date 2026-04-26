284e80d3771d23dd239a1b68e33fb0c0a0590d4d
Kshitij Singh <kshtjkmr10@gmail.com>
2026-04-26 14:33:26 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args){
@@ -18,6 +21,9 @@ public class Main {
           serverSocket.setReuseAddress(true);
           // Wait for connection from client.
           clientSocket = serverSocket.accept();
+          OutputStream outputStream = clientSocket.getOutputStream();
+          outputStream.write("+PONG\r\n".getBytes());
+          outputStream.flush();
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
